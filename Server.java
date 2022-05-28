36b7479eae20bcae21bb766950d2e3ef66a2a561
Dsanabria02 <100260303+Dsanabria02@users.noreply.github.com>
2022-05-27 22:51:31 -0600

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,40 @@
+
+package Client;
+
+import java.net.*;
+import java.io.*;
+import Common.*;
+/**
+ *
+ * @author Diana
+ */
+public class Server implements Runnable{
+    ServerSocket server;
+    Socket client;
+    ObjectInputStream input;
+    Dot dot;
+
+    public Server(Dot d){
+        dot = d;
+        try {
+            server = new ServerSocket(4450);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        
+    }
+    public void run(){
+        try {
+            while(true){
+                client = server.accept();
+                input = new ObjectInputStream(client.getInputStream());
+                dot.target = (Target)input.readObject();
+                input.close();
+                client.close();
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+    }
+}
